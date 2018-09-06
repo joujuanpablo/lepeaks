7dbc7073ae2bac138ab40fbed98c79bb1724bfef
Juan Pablo Jou <jjouvalencia@deloitte.co.uk>
2018-09-06 18:05:06 -0400

    Terms and conditions page

--- src/components/Story/Story.jsx
+++ src/components/Story/Story.jsx
@@ -9,6 +9,7 @@ export default (props) => {
             <HeroImage imageUrl={Image} heroHeight="300px">
             </HeroImage>
             Story Page
+            Talk about your musical history, your different musical groups, the transition to adulthood, sabbatical and realisation. The plan to escape.
             <img className="under-construction" src={UnderConstruction} alt="under construction"/>    
         </div>
     )
