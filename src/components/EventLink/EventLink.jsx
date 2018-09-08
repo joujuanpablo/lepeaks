e5e942878cf1581ea55af4428bc6e19398d1b2e0
Juan Pablo Jou <jjouvalencia@deloitte.co.uk>
2018-09-08 16:07:48 -0400

    Events responsiveness

--- src/components/EventLink/EventLink.jsx
+++ src/components/EventLink/EventLink.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import testImage from '../../images/venues/test-throne.jpg'
 
 class EventLink extends React.Component {
     render() {
@@ -7,24 +6,28 @@ class EventLink extends React.Component {
 
         return (
             <div className="event-summary">
-                <div className="wrapper">
-                    <div className="date col-md-2">
+                <div className="wrapper row">
+                    <div className="date col-2">
                         <div className="day">{date.day}</div>
                         <div className="month">{date.month}</div>
                         <div className="year">{date.year}</div>
                     </div>
-                    <div className="details-box col-md-7">
-                        <a className="venue" href={locationUrl}>{venue}, {city}</a>
-                        <div className="time">@ {date.time}</div>
-                        <div className="blurb">{blurb}</div>
-                    </div>
-                    <div className="buttons-box col-md-3">
-                        <a href={eventUrl} className="details button">Details</a>
-                        {
-                            tickets === "" ? <div className="free button">Free</div>
-                            : <a href={tickets} className="tickets button">Tickets</a>
-                            
-                        }
+                    <div className="info col-10">
+                        <div className="row">
+                            <div className="details-box col-12 col-sm-12 col-md-8 col-lg-9">
+                                <a className="venue" href={locationUrl}>{venue}, {city}</a>
+                                <div className="time">@ {date.time}</div>
+                                <div className="blurb">{blurb}</div>
+                            </div>
+                            <div className="buttons-box col-12 col-sm-12 col-md-4 col-lg-3">
+                                <a href={eventUrl} className="details button">Details</a>
+                                {
+                                    tickets === "" ? <div className="free button">Free</div>
+                                        : <a href={tickets} className="tickets button">Tickets</a>
+
+                                }
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
