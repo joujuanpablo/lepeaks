5731381537b4678ac8040fd968c54453e863feff
Juan Pablo Jou-Valencia <Jjouvalencia@uk-c02x344pjhd3.uk.deloitte.com>
2018-10-11 16:53:21 +0100

    WIP typescript migration

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -82,7 +82,7 @@ module.exports = {
     // https://github.com/facebookincubator/create-react-app/issues/290
     // `web` extension prefixes have been added for better support
     // for React Native Web.
-    extensions: ['.web.js', '.mjs', '.js', '.json', '.web.jsx', '.jsx'],
+    extensions: ['.web.js', '.mjs', '.js', '.json', '.web.jsx', '.jsx', '.ts', '.tsx'],
     alias: {
       
       // Support React Native Web
@@ -138,6 +138,7 @@ module.exports = {
               name: 'static/media/[name].[hash:8].[ext]',
             },
           },
+          { test: /\.tsx?$/, loader: "ts-loader" },
           // Process JS with Babel.
           {
             test: /\.(js|jsx|mjs)$/,
